b102f0584ceded71fef35270c449aa22e262256e
Jason Tolen <Jtolen543@gmail.com>
2024-04-10 12:03:08 -0400

    This is my new file

--- main.py
+++ main.py
@@ -1,3 +1,31 @@
-# This is my first github addition
+# This is my first GitHub addition
 
-print("hello world")
+def encode_password(choice):
+    temp = [int(i) for i in choice]
+    temp2 = [str(i+3) for i in temp]
+    choice = "".join(temp2)
+    return choice
+
+
+def main():
+    while True:
+        encode = 0
+        original = 0
+        print("Menu")
+        print("-------------")
+        print("1 Encode")
+        print("2. Decode")
+        print("3. Quit")
+        user = input("Please enter an option: ")
+        user = int(user)
+        if user == 1:
+            input1 = input("Please enter your password to encode: ")
+            encode = encode_password(input1)
+            original = input1
+            print("Your password has been encode and stored!")
+        elif user == 2:
+            print(f"The encoded password is {encode}, and the original password is {original})")
+        elif user == 3:
+            break
+
+main()
